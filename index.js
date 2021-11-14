d2037c642b8fcd125e9aff289ff69ff9bde52a20
Ankur Phani <contact.ankurphani@gmail.com>
2021-11-14 14:19:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { auth } = require("google-auth-library");
+const {google} = require("googleapis")
+PORT= 4747;
+const app = express();
+
+
+app.use(express.json());
+
+app.post("/testForm", async (req, res)=>{
+    
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    })
+
+    //create client instancce
+const client = await auth.getClient();
+
+//instance of google sheet api
+
+const googleSheets = google.sheets({version: "v4", auth: client});
+
+
+const spreadsheetId = "1ZgPvbpJhFT2kpGvIb1q3DeTPNzL848TYpMGlV5H4_4Q"
+
+// //get metadata
+// const metaData = await googleSheets.spreadsheets.get({
+//     auth,
+//     spreadsheetId,
+// })
+
+// //Read rows FROM sheet
+
+// const getRows = await googleSheets.spreadsheets.values.get({
+//     auth,
+//     spreadsheetId,
+//     range: "Subs!A:B"
+// })
+
+//Write rows TO sheet
+const {name, email, phone, city} = req.body;
+
+await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range:"Subs!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+        values: [
+            [name, email, phone, city]
+        ],
+    }
+})
+
+res.send("Response Submitted")
+});
+
+app.listen(PORT, (req,res)=>{
+    console.log("server running")
+})
