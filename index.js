d7ec6f41eae03df5955ba291aca17d3373bd4817
Ankur Phani <ankur@the-algo.com>
2021-12-25 14:22:43 +0530

    cors change

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var cors = require('cors')
 PORT = 4747;
 const app = express();
 
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
